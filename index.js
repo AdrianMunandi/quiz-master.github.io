08e4f57e6c207457207693acefea5f299fcede49
AdrianMunandi <adrian@ex.example.com>
2023-07-09 23:17:23 +0300

    Made some changes need some changes but almost done

--- index.js
+++ index.js
@@ -1,61 +1,66 @@
-// index.js
-
 // Fetch questions from the server
 function fetchQuestions() {
-    return fetch('db.json')
-      .then(response => response.json())
-      .then(data => data.questions);
-  }
+  return fetch('db.json')
+    .then(response => response.json())
+    .then(data => data.questions);
+}
 
-  // Display question and options
-  function displayQuestion(question) {
-    const questionTextElement = document.getElementById('questions');
-    const optionsListElement = document.querySelector('ul');
+// Display question and options
+function displayQuestion(question) {
+  const questionTextElement = document.getElementById('questions');
+  const optionsListElement = document.querySelector('ul');
 
-    questionTextElement.textContent = question.question;
+  questionTextElement.textContent = question.question;
 
-    const options = ['a', 'b', 'c', 'd'];
+  const options = ['a', 'b', 'c', 'd'];
 
-    options.forEach((option, index) => {
-      const optionInputElement = document.getElementById(option);
-      const optionLabelElement = document.getElementById(`${option}_text`);
-      optionInputElement.checked = false;
-      optionLabelElement.textContent = question.options[index];
-    });
-  }
+  options.forEach((option, index) => {
+    const optionInputElement = document.getElementById(option);
+    const optionLabelElement = document.getElementById(`${option}_text`);
+    optionInputElement.checked = false;
+    optionLabelElement.textContent = question.answers[option];
+  });
+}
 
-  // Submit quiz answers
-  function submitAnswers() {
-    const selectedOption = document.querySelector('input[name="answer"]:checked');
-    if (selectedOption) {
-      const selectedAnswerIndex = parseInt(selectedOption.value);
-      const currentQuestion = questions[currentQuestionIndex];
-      if (selectedAnswerIndex === currentQuestion.correctAnswer) {
-        score++;
-      }
-      currentQuestionIndex++;
-      if (currentQuestionIndex < totalQuestions) {
-        displayQuestion(questions[currentQuestionIndex]);
-      } else {
-        showResult();
-      }
+// Submit quiz answers
+function submitAnswers() {
+  const selectedOption = document.querySelector('input[name="answer"]:checked');
+  if (selectedOption) {
+    const selectedAnswerId = selectedOption.id;
+    const selectedAnswer = selectedAnswerId.charAt(selectedAnswerId.length - 1);
+    const currentQuestion = questions[currentQuestionIndex];
+    if (selectedAnswer === currentQuestion.correct) {
+      score++;
+    }
+    currentQuestionIndex++;
+    if (currentQuestionIndex < totalQuestions) {
+      displayQuestion(questions[currentQuestionIndex]);
+    } else {
+      showResult();
     }
   }
+}
 
-  // Fetch questions and start the quiz
-  let questions = [];
-  let currentQuestionIndex = 0;
-  let score = 0;
-  let totalQuestions = 0;
+// Display quiz result
+function showResult() {
+  const resultElement = document.getElementById('result');
+  resultElement.textContent = `You answered ${score} out of ${totalQuestions} questions correctly.`;
+  resultElement.style.display = 'block';
+}
 
-  fetchQuestions()
-    .then(data => {
-      questions = data;
-      totalQuestions = questions.length;
-      displayQuestion(questions[currentQuestionIndex]);
-    })
-    .catch(error => console.error('Error fetching quiz questions:', error));
+// Fetch questions and start the quiz
+let questions = [];
+let currentQuestionIndex = 0;
+let score = 0;
+let totalQuestions = 0;
 
-  // Event listener for submit button
-  const submitButton = document.getElementById('submit');
-  submitButton.addEventListener('click', submitAnswers);
+fetchQuestions()
+  .then(data => {
+    questions = data;
+    totalQuestions = questions.length;
+    displayQuestion(questions[currentQuestionIndex]);
+    const submitButton = document.getElementById('submit');
+    submitButton.addEventListener('click', submitAnswers);
+  })
+  .catch(error => console.error('Error fetching quiz questions:', error));
+
